94ae2e4f73153df2ccee41a4d78038691c79d95e
Can Göktas <can.goektas@gmail.com>
2017-10-27 15:57:56 +0200

    Temporarily disable Node version check

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
-const currentNodeVersion = process.versions.node;
-const semver = currentNodeVersion.split(".");
-const major = semver[0];
-
-if (major < 8) {
-  console.error(`You are running Node ${currentNodeVersion}.`);
-  console.error("Please update your version of Node to 8 or higher.");
-
-  process.exit(1);
-}
+// TODO: Find out minimal version of Node, uncomment the version check.
+// const currentNodeVersion = process.versions.node;
+// const semver = currentNodeVersion.split(".");
+// const major = semver[0];
+//
+// if (major < x) {
+//   console.error(`You are running Node ${currentNodeVersion}.`);
+//   console.error("Please update your version of Node to 8 or higher.");
+//
+//   process.exit(1);
+// }
 
 require("./toolkit");
