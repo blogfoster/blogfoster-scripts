4e81abbbde3d0fc3b18db4a0428be4f8ec20053f
Can Göktas <can.goektas@gmail.com>
2017-11-03 14:56:29 +0100

    Support --fix option for "lint" script

--- scripts/lint.js
+++ scripts/lint.js
@@ -4,15 +4,25 @@ const baseConfig = require(paths.selfESLintConfig);
 
 // not yet implemented
 const ignorePattern = undefined;
+const optionalArg = process.argv[3];
+const shouldFix = optionalArg === "--fix";
 
 const engine = new CLIEngine({
   baseConfig,
   ignorePattern,
   useEslintrc: false,
-  ignore: false
+  ignore: false,
+  fix: shouldFix
 });
+
 const report = engine.executeOnFiles([paths.projectSrc]);
 
+if (shouldFix) {
+  // write any fixes to disk
+  CLIEngine.outputFixes(report);
+}
+
+// print the linting results
 const formatter = engine.getFormatter("stylish");
 console.log(formatter(report.results));
 
