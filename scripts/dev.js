34c6f376000dccb6ddc7b80fc04a0887b10f662c
Can Göktas <can.goektas@gmail.com>
2017-11-07 10:24:53 +0100

    Implement the "dev" script

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,9 +1,18 @@
+const { spawn } = require("child_process");
 const webpack = require("webpack");
 const paths = require("../config/paths");
 const webpackConfig = require(paths.selfWebpackConfigDev);
 
 const compiler = webpack(webpackConfig);
 const watchOptions = {};
+let serverProcess;
+
+const createServerProcess = () =>
+  spawn("node", [paths.projectBuildIndexJs], {
+    env: process.env,
+    cwd: paths.projectSrc,
+    stdio: "inherit"
+  });
 
 compiler.watch(watchOptions, (err, stats) => {
   // handle webpack configuration errors
@@ -35,4 +44,18 @@ compiler.watch(watchOptions, (err, stats) => {
       colors: true
     })
   );
+
+  if (serverProcess) {
+    // If the child process is already running, kill it so we can restart the
+    // server on the same port
+    serverProcess.kill();
+    if (!serverProcess.killed) {
+      console.error("Server in child process could not be killed");
+
+      process.exit(1);
+    }
+  }
+
+  // Create the server in a child process
+  serverProcess = createServerProcess();
 });
