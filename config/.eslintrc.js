0757ea4c58a8410813b4f4e65a403b34dd6b76b8
Can Göktas <can.goektas@gmail.com>
2017-11-08 13:14:25 +0100

    Use eslint-config-blogfoster

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,19 +1,3 @@
 module.exports = {
-  // Use "babel-eslint" as a parser to support experimental language features
-  parser: "babel-eslint",
-  env: {
-    node: true
-  },
-  // Use the "airbnb" config but ignore rules about formatting
-  extends: ["airbnb", "prettier"],
-  // Turn off rules that are too strict
-  rules: {
-    "global-require": "off",
-    "no-console": "off",
-    "no-underscore-dangle": "off",
-    "import/no-dynamic-require": "off",
-    "import/newline-after-import": "off",
-    "import/prefer-default-export": "off",
-    "import/max-dependencies": "off"
-  }
+  extends: ["blogfoster"]
 };
