62f1e2758e753270ab43f76c2680329855a89cdc
Nicolai Schmid <nicolai@schmid.uno>
2018-01-23 16:16:03 +0100

    Updated the required engine version to 6.x, fixed not supported template literal
    * Updated the engine field in package to require at least 6.x
    * Implemented a version check before running the scripts and exiting if the engine version is below 6.x
    * Fixed error message for version failure to not use template literals since node version below 4.x don't support them and some users might try to run the scripts with a lower node version

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-// TODO: Find out minimal version of Node, uncomment the version check.
-// const currentNodeVersion = process.versions.node;
-// const semver = currentNodeVersion.split(".");
-// const major = semver[0];
-//
-// if (major < x) {
-//   console.error(`You are running Node ${currentNodeVersion}.`);
-//   console.error("Please update your version of Node to 8 or higher.");
-//
-//   process.exit(1);
-// }
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 6) {
+  console.error('You are running Node ' + currentNodeVersion + '.');
+  console.error('Please update your version of Node to 6 or higher.');
+
+  process.exit(1);
+}
 
 require('./blogfoster-scripts');
