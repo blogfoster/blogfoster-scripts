558a6fa370af2e5271be058d12292c63e7693c62
Can Göktas <can.goektas@gmail.com>
2018-02-19 14:03:53 +0100

    Turn off some ESLint rules

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -3,13 +3,16 @@ module.exports = {
   // Turn off rules that are too strict
   // TODO: Remove these rules when we updated eslint-config-blogfoster
   rules: {
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
     'global-require': 'off',
+    'import/max-dependencies': 'off',
+    'import/newline-after-import': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/no-named-as-default': 'off',
+    'import/prefer-default-export': 'off',
     'no-console': 'off',
     'no-underscore-dangle': 'off',
-    'import/no-dynamic-require': 'off',
-    'import/newline-after-import': 'off',
-    'import/prefer-default-export': 'off',
-    'import/max-dependencies': 'off',
     'prefer-template': 'off'
   }
 };
